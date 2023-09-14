510a5ed1067bac87d76769cf69326184e4ac18d5
Neutrongoodie <goodluck.uche@yahoo.com>
2023-09-14 12:05:59 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - C program that prints a line
+ *Description: C program that prints exactly Programming is like building a multilingual puzzle with put function
+ Return: Always 0 (Success)
+ */
+int main(void)
+	{
+    /* Print the first part of the message */
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
