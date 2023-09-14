bd83895367e84bf7d6434a88011f27ca64ab8675
Neutrongoodie <goodluck.uche@yahoo.com>
2023-09-14 12:13:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,20 +2,10 @@
 
 /**
  * main - Entry point of the program
- *
- * Description: This function is the entry point of the program.
- * It prints a multilingual message using the puts function.
- *
- * Return: Always 0 (Success)
+ * Return:0 (Success)
  */
 int main(void)
 {
-    /* Print the first part of the message */
-    puts("Programming is like building a multilingual puzzle,");
-
-    /* Print the second part of the message */
-    puts("followed by a new line.");
-
-    /* Return 0 to indicate successful execution */
+	puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
