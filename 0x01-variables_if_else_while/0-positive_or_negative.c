9d785997487ae98ef57ff5bdf09aa9c5ccf1d400
Neutrongoodie <goodluck.uche@yahoo.com>
2023-09-18 12:57:29 -0700

    positive negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,22 +3,21 @@
 #include <stdio.h>
 
 /**
- * main - Positive anything is better than negative nothing
- * Description: Complete the source code in order to print whether the number stored in the variable n is positive or negative.
- * Return: 0 (successful execution)
+ * main - Entry point 
+ * Description: Complete the source code in order to print whether the number stored in the variable n is positive or negative
+ * Return: 0 (indicate execution success)
  */
-
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        if (n > 0)
-                printf("%d is positive\n", n);
-        else if  (n == 0)
-                printf("%d is zero\n", n);
-        else if (n < 0)
-                printf("%d is negative\n", n);
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == o)
+		printf("%d is zero\n", n)
+	else
+		printf("%d is negative\n", n)
+	Retun (0)
 }
