50c14e5b3971c4227318019b77e5913b4a42fe81
Neutrongoodie <goodluck.uche@yahoo.com>
2023-09-19 14:13:27 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,24 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-    // Seed the random number generator
-    srand(time(0));
+int main(void)
+{
+	int n;
+	int m;
 
-    // Generate a random number between 0 and RAND_MAX
-    int n = rand();
+	for (m = 0; m < 9; m++)
+		for (n = m + 1; n < 10; n++)
+			putchar(m + '0');
+	putchar(n + '0');
+	if (m < 8 || n < 9)
+		putchar(',');
+	putchar(' ');
+	putchar('\n');
 
-    // Extract the last digit of n
-    int lastDigit = n % 10;
-
-    printf("The string Last digit of %d is ", n);
-    
-    if (lastDigit > 5)
-        printf("and is greater than 5\n");
-	else if (lastDigit == 0\)
-        printf("and is 0\n")
-	else 
-        printf("and is less than 6 and not 0\n");
-
-    return 0;
+	return (0);
 }
