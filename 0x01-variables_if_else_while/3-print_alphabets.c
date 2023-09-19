84e5f6dfceb1fd3887c837555188713e90c5626d
Neutrongoodie <goodluck.uche@yahoo.com>
2023-09-19 14:16:12 -0700

    print 3 alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the sizes of various data types.
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+int main(void)
+{
+        char letter;
+
+        /*prints lowercase letters*/
+        for (letter = 'a'; letter <= 'z'; letter++)
+        {
+                putchar(letter);
+        }
+
+        /*prints uppercase letters*/
+        for (letter = 'A'; letter <= 'Z'; letter++)
+        {
+                putchar(letter);
+        }
+        /*prints new line*/
+        putchar('\n');
+
+        return (0);
+
+
+}
