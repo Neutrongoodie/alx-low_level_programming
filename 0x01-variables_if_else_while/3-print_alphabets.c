4c5b574c8f98bdcf73cec96be894673166152bbf
Neutrongoodie <goodluck.uche@yahoo.com>
2023-09-19 14:20:46 -0700

    alphabet 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,30 +2,17 @@
 
 /**
  * main - Entry point of the program
- *
- * Description: This program prints the sizes of various data types.
- *
  * Return: Always 0 (indicating successful execution)
  */
 int main(void)
 {
         char letter;
 
-        /*prints lowercase letters*/
         for (letter = 'a'; letter <= 'z'; letter++)
-        {
                 putchar(letter);
-        }
-
-        /*prints uppercase letters*/
         for (letter = 'A'; letter <= 'Z'; letter++)
-        {
                 putchar(letter);
-        }
-        /*prints new line*/
         putchar('\n');
 
         return (0);
-
-
 }
