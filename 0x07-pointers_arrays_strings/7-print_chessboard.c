51fbab59e1ffabfaca948453984bcddf73ca858b
Neutrongoodie <goodluck.uche@yahoo.com>
2023-09-26 16:32:27 -0700

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <unistd.h>
 /**
  * print_chessboard - entry point
  * @a: input
@@ -10,10 +11,12 @@ void print_chessboard(char (*a)[8])
 	int i;
 	int j;
 
-	for (i =0; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-			putchar(a[i][j]);
-		putchar('\n');
+		{
+			printf("%c ", a[i][j]);
+		}
+		printf("\n");
 	}
 }
